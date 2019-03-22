3a64567321cae828b3947c0564edf68f0b4f51cc
baranberkay96 <baranberkay96@gmail.com>
2019-03-23 00:32:13 +0300

    init commit

---
+++ inferring-mu.py
@@ -0,0 +1,52 @@
+import pandas as pd 
+import math
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+""" Loads data from Udacity Inference Statisctics Course Data Portfolio"""
+df = pd.read_csv('./data/engagement-ratio.csv') 
+
+"""Get list of columns"""
+
+columns = df.columns.values.tolist()
+
+column_index = 0
+selected_column = columns[column_index]
+
+""" Dataframe to Series to sampling"""
+df[selected_column] = pd.to_numeric(df[selected_column], errors ='coerce')
+
+""" Sample Size"""
+n = 35
+
+""" Find population mean for control"""
+
+mu = df["EngagementRatio"].mean(skipna=True)
+
+""" Sampling """
+sm = df.sample(n=n)
+
+""" Find sample mean and standard deviation for calculate lower and upper bound depends on give confidence interval"""
+mean = sm["EngagementRatio"].mean(skipna=True)
+sd = sm["EngagementRatio"].std(skipna=True)
+
+""" Determine confidence interval """
+ci = 0.95
+
+""" Find z-value for given ci"""
+z = stats.norm.ppf(ci)
+
+""" Calculate margin of error """
+moe = z * (sd / math.sqrt(n))
+
+""" Calculate lower and upper bound"""
+lower = mean - moe 
+upper = mean + moe
+
+
+print(f'We are {ci} confident population mean lies between {lower} and {upper}')
+print(f'True mean is {mu}')
+
+
